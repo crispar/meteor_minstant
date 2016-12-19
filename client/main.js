cd933f2aa1856d22bba962116143597f979c3910
jungwook Park <jwcris.park@gmail.com>
2016-12-19 13:49:45 +0000

    1st Assignment : Devide file and add avartar on chat screen

---
+++ client/main.js
@@ -0,0 +1,127 @@
+///
+// helper functions
+///
+Template.available_user_list.helpers({
+    users:function(){
+        return Meteor.users.find();
+    }
+})
+
+Template.available_user.helpers({
+    getUsername:function(userId){
+        user = Meteor.users.findOne({_id:userId});
+        return user.profile.username;
+    },
+    isMyUser:function(userId){
+        if (userId == Meteor.userId()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    },
+    isLogon:function() {
+        //console.log(Meteor.userId());
+        if (Meteor.userId()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+})
+
+Template.chat_page.helpers({
+    messages:function(){
+      var chat = Chats.findOne({_id:Session.get("chatId")});
+      return chat.messages;
+    }, 
+    other_user:function(){
+      return ""
+    },
+
+    find_opponent: function(isName) {
+        var opponentUser;
+        var chat = Chats.findOne({_id:Session.get("chatId")});
+        console.log("1.1." + chat.user1Id);
+        if(chat.user1Id == Meteor.userId()) {
+            opponentUser = chat.user2Id;
+        } else {
+            opponentUser = chat.user1Id;
+        }
+        console.log("1.2." + opponentUser);
+        console.log("1.3." + Meteor.users.findOne({
+            _id: opponentUser
+            }));
+        var profiles = Meteor.users.findOne({_id: opponentUser});
+        if ( profiles != undefined) {
+            if (isName == true) {
+                return Meteor.users.findOne({
+                _id: opponentUser
+                }).profile.username;
+            } else {
+                return Meteor.users.findOne({
+                _id: opponentUser
+                }).profile.avatar;
+            }
+        }
+    },
+
+    find_me: function(isName) {
+        var myUser;
+        var chat = Chats.findOne({_id:Session.get("chatId")});
+        console.log("2.1." + chat.user1Id);
+        if(chat.user1Id == Meteor.userId()) {
+            myUser = chat.user1Id;
+        } else {
+            myUser = chat.user2Id;
+        }
+        console.log("2.2." + myUser);
+        console.log("2.3." + Meteor.users.findOne({
+            _id: myUser
+            }));
+        var profiles = Meteor.users.findOne({_id: myUser});
+        if ( profiles != undefined) {
+            if (isName == true) {
+                return Meteor.users.findOne({
+                  _id: myUser
+                }).profile.username;
+            } else {
+                return Meteor.users.findOne({
+                  _id: myUser
+                }).profile.avatar;
+            }
+        }
+    }
+})
+
+Template.chat_page.events({
+    // this event fires when the user sends a message on the chat page
+    'submit .js-send-chat':function(event){
+        // stop the form from triggering a page reload
+        event.preventDefault();
+        // see if we can find a chat object in the database
+        // to which we'll add the message
+        var chat = Chats.findOne({_id:Session.get("chatId")});
+        if (chat){// ok - we have a chat to use
+            var msgs = chat.messages; // pull the messages property
+            if (!msgs){// no messages yet, create a new array
+                msgs = [];
+            }
+            var user = Meteor.users.findOne({_id:Meteor.user()._id})
+            var username = user.profile.username;
+            // is a good idea to insert data straight from the form
+            // (i.e. the user) into the database?? certainly not. 
+            // push adds the message to the end of the array
+            msgs.push({
+              user: username,
+              text: event.target.chat.value
+            });
+            // reset the form
+            event.target.chat.value = "";
+            // put the messages array onto the chat object
+            chat.messages = msgs;
+            // update the chat object in the database.
+            Chats.update(chat._id, chat);
+        }
+    }
+})
